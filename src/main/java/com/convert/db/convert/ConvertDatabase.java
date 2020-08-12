898dff06d1cb81aa2661dcaeb8fd843d718d2d4a
C02793 <c02793@g-able.com>
2020-08-12 11:20:46 +0700

    Prototype of convertor by testing convert nvl to coalesce

--- src/main/java/com/convert/db/convert/ConvertDatabase.java
+++ src/main/java/com/convert/db/convert/ConvertDatabase.java
@@ -17,7 +17,8 @@ public class ConvertDatabase {
 		Scanner scanner = null;
 
 		try {
-			Path pathFile = Paths.get(args[0]);
+//			Path pathFile = Paths.get(args[0]);
+			Path pathFile = Paths.get("O:\\Workspaces\\convert-db\\convert\\plaintext.txt");
 
 			if (Files.exists(pathFile)) {
 				// read text file
@@ -25,7 +26,7 @@ public class ConvertDatabase {
 				scanner = new Scanner(inputStream, "UTF-8");
 
 				boolean convertDB = false;
-				String regex = "\\([^0][0-9]* rows affected\\)";
+//				String regex = "\\([^0][0-9]* rows affected\\)";
 				
 				List<String> listOracleSyntax = new ArrayList<String>();
 
@@ -34,13 +35,15 @@ public class ConvertDatabase {
 
 				final CharSequence[] charSeqOracleItems = listOracleSyntax.toArray(new CharSequence[listOracleSyntax.size()]);
 				
-
 				while (scanner.hasNextLine()) {
 					String line = scanner.nextLine();
-					if (StringUtils.isNotBlank(line)) {
+					if (StringUtils.isNotEmpty(line)) {
 						for (CharSequence charseq : charSeqOracleItems) {
 							if (line.toLowerCase().contains(charseq)) {
-								convertDB = true;
+//								convertDB = true;
+								line = convert(line, charseq);
+								System.out.println("main");
+								System.out.println(line);
 							}
 						}
 					} else {
@@ -48,9 +51,25 @@ public class ConvertDatabase {
 					}
 				}
 				
+				
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static String convert(String line, CharSequence charOracle) {
+		String stringConverted = "";
+		line = line.toLowerCase();
+		//charOracle.charAt(charOracle.length()-1) index of charAt is begin from 0, if length = 3 so index = [0, 1, 2]
+		int firstIndexCharOracle = line.indexOf(charOracle.charAt(0));
+		int lastIndexCharOracle = line.indexOf(charOracle.charAt(charOracle.length()-1));
+		
+		String remainStr = line.substring(firstIndexCharOracle, lastIndexCharOracle+1);
+		if (StringUtils.equals(remainStr, "nvl")) {
+			stringConverted = line.replace(remainStr, "coalesce");
+		}
+		
+		return stringConverted;
 	}
 }

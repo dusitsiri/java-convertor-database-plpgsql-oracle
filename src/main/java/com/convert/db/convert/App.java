185fc348e2922996c7017ac02718ce6e98163b1b
C02793 <c02793@g-able.com>
2020-08-17 23:29:43 +0700

    Update find index oracle syntax solutioin

--- src/main/java/com/convert/db/convert/App.java
+++ src/main/java/com/convert/db/convert/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String string = "decode (a.tf_status ,'NTF','NT','TF','TR','TR'  ) tf_status ,";
+    	
+    	String[] decodeArry = string.substring(string.indexOf("(")+1, string.indexOf(")")).split(",");
+    	
+    	for (int i = 0; i < decodeArry.length; i++) {
+			String string1 = decodeArry[i];
+			System.out.println(string1);
+		}
     }
 }
--- src/main/java/com/convert/db/convert/ConvertDatabase.java
+++ src/main/java/com/convert/db/convert/ConvertDatabase.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
 import org.apache.commons.lang3.StringUtils;
 
 public class ConvertDatabase {
+	
 	public static void main(String[] args) {
 
 		FileInputStream inputStream = null;
@@ -20,9 +21,9 @@ public class ConvertDatabase {
 
 		try {
 //			Path pathFile = Paths.get(args[0]);
-//			Path pathFile = Paths.get("O:\\Workspaces\\convert-db\\convert\\plaintext.txt");
-			Path pathFile = Paths.get("D:/Coding/java-convertor-database-plpgsql-oracle/plaintext.txt");
-//			Path pathFile = Paths.get("O:\\PL\\decode.txt");
+			Path pathFile = Paths.get("O:/Workspaces/convert-db/convert/plaintext.txt");
+//			Path pathFile = Paths.get("D:/Coding/java-convertor-database-plpgsql-oracle/plaintext.txt");
+//			Path pathFile = Paths.get("O:/PL/decode.txt");
 
 			if (Files.exists(pathFile)) {
 				// read text file
@@ -43,7 +44,7 @@ public class ConvertDatabase {
 					if (StringUtils.isNotEmpty(line)) {
 						for (CharSequence charseq : charSeqOracleItems) {
 							if (line.toLowerCase().contains(charseq)) {
-								line = convert(line, charseq);
+								line = findCharOracle(line, charseq);
 							}
 						}
 						afterConvertString = afterConvertString + line + "\n";
@@ -60,31 +61,50 @@ public class ConvertDatabase {
 		}
 	}
 	
-	public static String convert(String line, CharSequence charOracle) {
+	//find index of oracle syntax
+	public static String findCharOracle(String line, CharSequence charOracle) {
 		String stringConverted = "";
 		line = line.toLowerCase();
 
-		//charOracle.charAt(charOracle.length()-1) index of charAt is begin from 0, if length = 3 so index = [0, 1, 2]
-		int firstIndexCharOracle = line.indexOf(charOracle.charAt(0));
-		int lastIndexCharOracle = line.indexOf(charOracle.charAt(charOracle.length()-1));
+		//charOracle.toString().length()-1 index of charAt is begin from 0, if length = 3 so index = [0, 1, 2]
+		int firstIndexCharOracle = line.indexOf(charOracle.toString());
+		int lastIndexCharOracle = firstIndexCharOracle+(charOracle.toString().length()-1);
+		
 		
 		String remainStr = line.substring(firstIndexCharOracle, lastIndexCharOracle+1);
+		
 		if (StringUtils.equals(remainStr, "nvl")) {
-			stringConverted = line.replace(remainStr, "coalesce");
+			stringConverted = convertNvl(remainStr, line);
 		} else if (StringUtils.equals(remainStr, "decode")) {
-			//1.normal case 2.more one line 
-			stringConverted = line.replace(remainStr, "case");
+			stringConverted = convertDecode(remainStr, line);			
 		}
 		
 		return stringConverted;
 	}
 	
+	//nvl
+	public static String convertNvl(String remainStr, String line) {
+		String stringConverted = "";
+		stringConverted = line.replace(remainStr, "coalesce");
+		return stringConverted;
+	}
+	
+	//decode
+	public static String convertDecode(String remainStr, String line) {
+		String stringConverted = "";
+		//1.normal case 2.more one line 
+		stringConverted = line.replace(remainStr, "case");
+		String[] decodeArry = stringConverted.substring(stringConverted.indexOf("(")+1, stringConverted.indexOf(")")).split(",");
+		return stringConverted;
+	}
+	
+	//write file
 	public static void writeFile(String text) throws IOException {
 		
 		try {
 			
-//			FileOutputStream out = new FileOutputStream("O:/Workspaces/convert-db/convert/newPlainText.txt");
-			FileOutputStream out = new FileOutputStream("D:/Coding/java-convertor-database-plpgsql-oracle/newPlainText.txt");
+			FileOutputStream out = new FileOutputStream("O:/Workspaces/convert-db/convert/newPlainText.txt");
+//			FileOutputStream out = new FileOutputStream("D:/Coding/java-convertor-database-plpgsql-oracle/newPlainText.txt");
 			out.write(text.getBytes());
 			
 		} catch (IOException e) {
